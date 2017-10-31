6294d3e09e5c78f67b7ed36c6a11a01301018d57
Chris Crewdson <chriscrewdson@gmail.com>
2017-10-30 18:05:44 -0700

    back to where it should have been

--- routes/search.js
+++ routes/search.js
@@ -5,60 +5,39 @@ const request = require('request-promise');
 exports.get = function (req, res) {
     var limiter = new bottleneck(1, 1000);
 
-    async.mapSeries(itemIds, function(itemId, callback) {
-        limiter.schedule(getItem, itemId, req.query.term, callback);
-    },
-    function(err, mapRes) {
-        if (err) {
-            console.log(err);
-        }
-        return res(mapRes.filter(function(current){
-            return current != null;
-        }));
-    })
+    return getItem(1, req.query.title, function (json) {
+        //pull out additional pages
+
+        var page_numbers = Array.apply(null, Array(json.total_pages)).map(function (_, i) {return i;});
+
+        async.mapSeries(page_numbers, function(page, title, callback) {
+            limiter.schedule(getItem, page, title, callback);
+        },
+        function(err, mapRes) {
+            if (err) {
+                console.log(err);
+            }
+            return res(mapRes);
+        })
+    });
 }
 
-const getItem = function(itemId, term, callback) {
-    return request({uri: upstreamUri(itemId), json: true})
+const getItem = function(page, title, callback) {
+    return request({uri: upstreamUri(title, page), json: true})
     .then(function (json) {
+        var results = [];
         // console.log(`got item id ${itemId}`);
-        // console.log(`shortDescription: ${json.shortDescription.toLowerCase()}`);
-        if (json.shortDescription.toLowerCase().indexOf(term.toLowerCase()) > -1 ||
-            json.longDescription.toLowerCase().indexOf(term.toLowerCase()) > -1
-        ) {
-            //console.log('found match');
-            callback(null, itemId);
-        } else {
-            callback();
-        }
+        json.data.forEach((movie) => {
+            results.push(movie.Title);
+        });
+        callback(null, results);
     })
     .catch(function (err) {
-        console.log(`failure getting item id ${itemId}: ${err}`);
+        console.log(`failure getting page ${page}: ${err}`);
         callback(); // Don't shortcut getting a partial search result
     });
 }
 
-const upstreamUri = function(itemId) {
-    return `http://api.walmartlabs.com/v1/items/${itemId}?format=json&apiKey=kjybrqfdgp3u4yv2qzcnjndj`
+const upstreamUri = function(title, page) {
+    return `https://jsonmock.hackerrank.com/api/movies/search?title=${title}&page=${page}`
 }
-
-const itemIds = [
-    14225185,
-    14225186,
-    14225188,
-    14225187,
-    39082884,
-    30146244,
-    12662817,
-    34890820,
-    19716431,
-    42391766,
-    35813552,
-    40611708,
-    40611825,
-    36248492,
-    44109840,
-    23117408,
-    35613901,
-    42248076
-];
