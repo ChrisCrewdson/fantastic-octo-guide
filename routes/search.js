0c13e695c78f0f4a6137f1326c68df9ef5d2dfbb
Chris Crewdson <chriscrewdson@gmail.com>
2017-10-30 19:11:38 -0700

    basic implementation finished

--- routes/search.js
+++ routes/search.js
@@ -1,40 +1,49 @@
 const async = require('async');
-const bottleneck = require("bottleneck");
 const request = require('request-promise');
+const _ = require('lodash');
 
 exports.get = function (req, res) {
-    var limiter = new bottleneck(1, 1000);
-
-    return getItem(1, req.query.title, function (json) {
-        //pull out additional pages
-
-        var page_numbers = Array.apply(null, Array(json.total_pages)).map(function (_, i) {return i;});
-
-        async.mapSeries(page_numbers, function(page, title, callback) {
-            limiter.schedule(getItem, page, title, callback);
-        },
-        function(err, mapRes) {
-            if (err) {
-                console.log(err);
-            }
-            return res(mapRes);
-        })
-    });
-}
-
-const getItem = function(page, title, callback) {
-    return request({uri: upstreamUri(title, page), json: true})
+    return request({uri: upstreamUri(req.query.title, 1), json: true})
     .then(function (json) {
+        
         var results = [];
-        // console.log(`got item id ${itemId}`);
         json.data.forEach((movie) => {
             results.push(movie.Title);
         });
-        callback(null, results);
-    })
-    .catch(function (err) {
-        console.log(`failure getting page ${page}: ${err}`);
-        callback(); // Don't shortcut getting a partial search result
+
+        if (json.total_pages <= 1) {
+            return res(results)
+        } else {
+            console.log(`will get ${json.total_pages} pages`)
+            var page_numbers = _.range(2, json.total_pages + 1)
+
+            async.map(
+                page_numbers,
+                function(page, callback) {
+                    return request({uri: upstreamUri(req.query.title, page), json: true})
+                    .then(function (json) {
+                        var results = [];
+
+                        json.data.forEach((movie) => {
+                            results.push(movie.Title);
+                        });
+                        callback(null, results);
+                    })
+                },
+                function(err, mapRes) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    mapRes.forEach((pageResult)=> {
+                        pageResult.forEach((title) => {
+                            results.push(title);
+                        });
+                    });
+
+                    return res([...new Set(results)].sort());
+                }
+            );
+        }
     });
 }
 
